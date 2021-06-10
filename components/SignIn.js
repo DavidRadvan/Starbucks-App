0de7e78b6665da1a53f04bbdc9d67a1d5eea0dbb
David Radvan <david.radvan@gmail.com>
2021-06-09 19:08:43 -0700

    add forms and validation to signin/signup

--- components/SignIn.js
+++ components/SignIn.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button, TextInput, Alert } from 'react-native';
 
 export default function SignIn(props) {
+
+  const [email, onChangeEmail] = useState("");
+  const [password, onChangePassword] = useState("");
+
+  const validate = function() {
+
+    if (email === "" || password === "") {
+      Alert.alert("Error", "Field left blank")
+      return;
+    }
+
+    // save()
+  }
+
   return (
     <View>
-    <Button
-      title="Back to Home"
-      onPress={props.backHome}
-    />
-      <Text> Sign In </Text>
+      <Button
+        title="Back to Home"
+        onPress={props.backHome}
+      />
+      <TextInput
+        value={email}
+        onChangeText={onChangeEmail}
+        placeholder="Email"
+      />
+      <TextInput
+        value={password}
+        onChangeText={onChangePassword}
+        placeholder="Password"
+      />
+      <Button
+        title="Submit"
+        onPress={() => validate()}
+      />
     </View>
   );
 }
--- components/SignUp.js
+++ components/SignUp.js
@@ -1,14 +1,53 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button, Alert, TextInput } from 'react-native';
 
 export default function SignIn(props) {
+
+  const [firstName, onChangeFirstName] = useState("");
+  const [lastName, onChangeLastName] = useState("");
+  const [email, onChangeEmail] = useState("");
+  const [password, onChangePassword] = useState("");
+
+  const validate = function() {
+
+    if (email === "" || password === "") {
+      Alert.alert("Error", "Field left blank")
+      return;
+    }
+
+    // save()
+  }
+
   return (
     <View>
-    <Button
-      title="Back to Home"
-      onPress={props.backHome}
-    />
-      <Text> Sign Up </Text>
+      <Button
+        title="Back to Home"
+        onPress={props.backHome}
+      />
+      <TextInput
+        value={firstName}
+        onChangeText={onChangeFirstName}
+        placeholder="First name"
+      />
+      <TextInput
+        value={lastName}
+        onChangeText={onChangeLastName}
+        placeholder="Last name"
+      />
+      <TextInput
+        value={email}
+        onChangeText={onChangeEmail}
+        placeholder="Email"
+      />
+      <TextInput
+        value={password}
+        onChangeText={onChangePassword}
+        placeholder="Password"
+      />
+      <Button
+        title="Submit"
+        onPress={() => validate()}
+      />
     </View>
   );
 }
